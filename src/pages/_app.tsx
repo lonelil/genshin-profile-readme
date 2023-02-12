9f27af4e0200024d3ca5eb21ae993ffde8324365
lonelil <51315646+lonelil@users.noreply.github.com>
2023-02-12 23:26:34 +0800

    added tailwind

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Genshin Impact Profile Readme</title>
+      </Head>
+      <main className="min-h-screen">
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
 }
